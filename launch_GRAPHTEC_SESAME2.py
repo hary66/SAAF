9ca8642b1d8343cff58914f83fa269801965a4ad
hary66 <bartibox-space@yahoo.fr>
2017-04-03 17:46:22 +0200

    Lancement SESAME avec paramètres fonctionnel

--- launch_GRAPHTEC_SESAME2.py
+++ launch_GRAPHTEC_SESAME2.py
@@ -16,52 +16,52 @@ subprocess.Popen([r"C:\Program Files (x86)\Graphtec\GL100_240_840-APS\GL100_240_
 print("GRAPHTEC se lance")
 #time.sleep(4)
 
-##while pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Start_the_standard_mode.png') == None:
+##while pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Start_the_standard_mode.png') == None:
 ##    print('attente1')
 ##    time.sleep(0.5)
 
-##while pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Start_the_standard_mode.png') != None:
+##while pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Start_the_standard_mode.png') != None:
 ##    print("GRAPHTEC démarre")
     
-while pyautogui.locateOnScreen(r"C:\Users\Harold\Documents\Reine\Python\not_connected.png") ==None: #wait until the nice window to come
+while pyautogui.locateOnScreen(r"C:\Python35-32\Launch_Graphtec\icones_Graphtec\not_connected.png") ==None: #wait until the nice window to come
     print("waiting until the nice window to come to click on the right button !")
     time.sleep(9)
     
-if pyautogui.locateOnScreen(r"C:\Users\Harold\Documents\Reine\Python\not_connected.png") !=None:
+if pyautogui.locateOnScreen(r"C:\Python35-32\Launch_Graphtec\icones_Graphtec\not_connected.png") !=None:
     print("GRAPHTEC en attente de connection")
     pyautogui.press('f5')
-while pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\a_connecter.PNG')  ==None:
+while pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\a_connecter.PNG')  ==None:
     print("GRAPHTEC connexion en cours")
     time.sleep(0.5)
     
 
-connect_button = pyautogui.locateCenterOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Connect_F5.png')
+connect_button = pyautogui.locateCenterOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Connect_F5.png')
 if connect_button != None:
     pyautogui.click(connect_button)
 
-while pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Connected.PNG') ==None:
+while pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Connected.PNG') ==None:
     print("GRAPHTEC pas encore connecté, mais bientot !")
     time.sleep(0.25)
 
-if pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Connected.PNG') !=None:
+if pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Connected.PNG') !=None:
     print("GRAPHTEC connecté")
-    pyautogui.click(pyautogui.locateCenterOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Close.PNG'))
+    pyautogui.click(pyautogui.locateCenterOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Close.PNG'))
 
-while pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Start.png') ==None:
+while pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Start.png') ==None:
     time.sleep(1)
 
-Start_png = pyautogui.locateCenterOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Start.png')
+Start_png = pyautogui.locateCenterOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Start.png')
 #print (Start_png)
 pyautogui.click(Start_png)
 print('Lancement de l\'enregistrement du GRAPHTEC')
 #print('Clické sur Start.png')
 
-confirmation_enregistrement_en_cours_R = pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_R.png')
-confirmation_enregistrement_en_cours_B = pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_B.png')
-while (pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_R.png')) == None:
-#while ((pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_R.png')) || (pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_B.png'))) ==None:
+confirmation_enregistrement_en_cours_R = pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_R.png')
+confirmation_enregistrement_en_cours_B = pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_B.png')
+while (pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_R.png')) == None:
+#while ((pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_R.png')) || (pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_B.png'))) ==None:
     time.sleep(0.25)
-if pyautogui.locateOnScreen(r'C:\Users\Harold\Documents\Reine\Python\Recording_R.png') != None:
+if pyautogui.locateOnScreen(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Recording_R.png') != None:
     print('Enregistrement GRAPHTEC en cours')    
 
 parabox = pyautogui.confirm(text=parametres, title='Confirmez vous les parametres SESAME', buttons=['OK', 'Cancel'])
@@ -69,6 +69,12 @@ print (parabox)
 
 if parabox =="OK":
     print('Lancement du SESAME avec les paramètres suivant : ' + parametres)
-    subprocess.check_output(['c:\Program Files (x86)\EGSesameTriangle\Debug\EGSesameTriangle.exe', '23.5', '0.25', '47', '73', '7.8'])
+    par=parametres.split(' ')
+    print (par)
+    print(par[1])
+    commande =["c:\Program Files (x86)\EGSesameTriangle\Debug\EGSesameTriangle.exe", par[0], par[1], par[2], par[3], par[4]]
+    print (commande)
+    subprocess.call(commande)
 elif parabox == 'Cancel':
     print("Arretez tout !")
+    pyautogui.click(r'C:\Python35-32\Launch_Graphtec\icones_Graphtec\Stop.png')
